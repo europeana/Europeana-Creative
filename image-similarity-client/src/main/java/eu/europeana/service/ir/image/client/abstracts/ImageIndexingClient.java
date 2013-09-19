629644e2750dd03a31842fbf17f46e841d5eb42f
Sergiu Gordea <sergiu.gordea@ait.ac.at>
2013-09-19 15:10:30 +0200

    added api 2 parent pom

--- image-similarity-client/src/main/java/eu/europeana/service/ir/image/client/abstracts/ImageIndexingClient.java
+++
@@ -1,5 +0,0 @@
-package eu.europeana.service.ir.image.client.abstracts;
-
-public interface ImageIndexingClient {
-
-}
--- image-similarity-client/src/main/java/eu/europeana/service/ir/image/client/abstracts/ImageSearchingClient.java
+++
@@ -1,7 +0,0 @@
-package eu.europeana.service.ir.image.client.abstracts;
-
-public interface ImageSearchingClient {
-
-	public abstract String getComponentNameFromRest();
-
-}
