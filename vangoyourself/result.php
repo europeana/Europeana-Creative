4aa72986a78cc7c91ba98042a785983c69985696
Bojan Božić <bojan.bozic@ait.ac.at>
2014-07-26 15:58:24 +0200

    updates and fixes

--- vangoyourself/result.php
+++ vangoyourself/result.php
@@ -1,42 +1,5 @@
 <head>
-<meta charset="UTF-8">
-<meta content="width=device-width" name="viewport">
 <title>Test | Van Go Yourself</title>
-<link href="http://gmpg.org/xfn/11" rel="profile">
-<link href="http://localhost/wordpress/wordpress/trunk/xmlrpc.php" rel="pingback">
-<!--[if lt IE 9]>
-<script src="http://localhost/wordpress/wordpress/trunk/wp-content/themes/twentyfourteen/js/html5.js"></script>
-<![endif]-->
-<meta content="noindex,follow" name="robots">
-<link href="http://localhost/wordpress/wordpress/trunk/?feed=rss2" title="Van Go Yourself » Feed" type="application/rss+xml" rel="alternate">
-<link href="http://localhost/wordpress/wordpress/trunk/?feed=comments-rss2" title="Van Go Yourself » Comments Feed" type="application/rss+xml" rel="alternate">
-<link href="http://localhost/wordpress/wordpress/trunk/?feed=rss2&amp;p=9" title="Van Go Yourself » Test Comments Feed" type="application/rss+xml" rel="alternate">
-<link media="all" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&amp;subset=latin%2Clatin-ext&amp;ver=3.9-alpha-27445" id="open-sans-css" rel="stylesheet">
-<link media="all" type="text/css" href="http://localhost/wordpress/wordpress/trunk/wp-includes/css/dashicons.min.css?ver=3.9-alpha-27445" id="dashicons-css" rel="stylesheet">
-<link media="all" type="text/css" href="http://localhost/wordpress/wordpress/trunk/wp-includes/css/admin-bar.min.css?ver=3.9-alpha-27445" id="admin-bar-css" rel="stylesheet">
-<link media="all" type="text/css" href="//fonts.googleapis.com/css?family=Lato%3A300%2C400%2C700%2C900%2C300italic%2C400italic%2C700italic" id="twentyfourteen-lato-css" rel="stylesheet">
-<link media="all" type="text/css" href="http://localhost/wordpress/wordpress/trunk/wp-content/themes/twentyfourteen/genericons/genericons.css?ver=3.0.2" id="genericons-css" rel="stylesheet">
-<link media="all" type="text/css" href="http://localhost/wordpress/wordpress/trunk/wp-content/themes/twentyfourteen/style.css?ver=3.9-alpha-27445" id="twentyfourteen-style-css" rel="stylesheet">
-<!--[if lt IE 9]>
-<link rel="stylesheet" id="twentyfourteen-ie-css"  href="http://localhost/wordpress/wordpress/trunk/wp-content/themes/twentyfourteen/css/ie.css?ver=20131205" type="text/css" media="all" />
-<![endif]-->
-<script src="http://localhost/wordpress/wordpress/trunk/wp-includes/js/jquery/jquery.js?ver=1.11.0" type="text/javascript"></script>
-<script src="http://localhost/wordpress/wordpress/trunk/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1" type="text/javascript"></script>
-<link href="http://localhost/wordpress/wordpress/trunk/xmlrpc.php?rsd" title="RSD" type="application/rsd+xml" rel="EditURI">
-<link href="http://localhost/wordpress/wordpress/trunk/wp-includes/wlwmanifest.xml" type="application/wlwmanifest+xml" rel="wlwmanifest">
-<meta content="WordPress 3.9-alpha-27445" name="generator">
-<link href="http://localhost/wordpress/wordpress/trunk/?p=9" rel="canonical">
-<link href="http://localhost/wordpress/wordpress/trunk/?p=9" rel="shortlink">
-<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
-<style media="print" type="text/css">#wpadminbar { display:none; }</style>
-<style media="screen" type="text/css">
-html { margin-top: 32px !important; }
-* html body { margin-top: 32px !important; }
-@media screen and ( max-width: 782px ) {
-	html { margin-top: 46px !important; }
-	* html body { margin-top: 46px !important; }
-}
-</style>
 </head>
 
 <?php
@@ -57,7 +20,18 @@ $image2 = new Imagick($target_path2);
 $httppath = 'http://' . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']);
 
 $geo1 = $image1->getimagegeometry();
-$image2->cropimage($width, $height, $x, $y);
+if(!($width == "" && $height == "" && $x == "" && $y == ""))
+{
+	if($width == "")
+	{$width = $geo1['width'];}
+	if($height == "")
+	{$height = $geo1['height'];}
+	if($x == "")
+	{$x = 0;}
+	if($y == "")
+	{$y = 0;}
+	$image2->cropimage($width, $height, $x, $y);
+}
 $geo2 = $image2->getimagegeometry();
 
 session_start();
--- vangoyourself/vangoyourself.php
+++ vangoyourself/vangoyourself.php
@@ -6,7 +6,18 @@ Version: 1.0.0
 Author: Bojan Božić and Sergiu Gordea
 Author URI: http://www.unet.univie.ac.at/~a0963121
 */
+
+/**
+ * Creates an input form for image uploads
+ */
+function change_title($post_object) {
+	$post_object->post_title = "Van Go Yourself";
+}
+
+function image_uploads() {
+	
 ?>
+
 <head>
 <title>Van Go Yourself</title>
 </head>
@@ -24,13 +35,10 @@ function selection() {
 	alarm("selection");
 }
 </script>
-			
-<h1><?php echo "Van Go Yourself" ?> </h1>
-<hr>
-<form enctype="multipart/form-data" action="result.php" method="POST">
+<form enctype="multipart/form-data" action="wp-content/plugins/vangoyourself/result.php" method="POST">
 <table>
-<tr><td>Original image:</td><td><input type="file" name="file1" onchange="fileupload(this.value)"></td></tr>
-<tr><td>Reenactment:</td><td><input type="file" name="file2"></td></tr>
+<tr><td>Original image*:</td><td><input type="file" name="file1" onchange="fileupload(this.value)"></td></tr>
+<tr><td>Reenactment*:</td><td><input type="file" name="file2"></td></tr>
 </table>
 <hr>
 <!--iframe id="target" frameborder=0 src="preview.html" style="width:100%; height:25%;">
@@ -39,7 +47,7 @@ function selection() {
 <tr>Selection coordinates:</tr>
 <tr><td>X:</td><td><input type="text" name="x"></td><td>Width:</td><td><input type="text" name="width"></td></tr>
 <tr><td>Y:</td><td><input type="text" name="y"></td><td>Height:</td><td><input type="text" name="height"></td></tr>
-<tr><td>Thumbnail width:</td><td><input type="text" name="th_width"></td></tr>
+<tr><td>Thumbnail width*:</td><td><input type="text" name="th_width"></td></tr>
 </table>
 <hr>
 <table>
@@ -49,26 +57,27 @@ function selection() {
 <tr><td>Short URL:</td><td><input type="text" name="txt_shorturl"></td></tr> 
 </table>
 <hr>
-<input type="radio" name="hv" value="Horizontal">Landscape<br>
+Orientation*:<br>
+<input type="radio" name="hv" value="Horizontal" checked>Landscape<br>
 <input type="radio" name="hv" value="Vertical">Portrait
 <hr>
-Color code: <input type="text" name="color_code"><br>
-Thickness: <input type="text" name="thickness"><br>
+Framing*:<br>
+<input type="radio" name="frame" value="Color Frame">Color Frame<br>
+<input type="radio" name="frame" value="Standard Frame" checked>Standard Frame<br>
+<input type="radio" name="frame" value="Image Frame">Image Frame<br>
+<input type="radio" name="frame" value="No Frame">No Frame<br>
+Color code: <input type="text" name="color_code" value="#FFFFFF"><br>
+Thickness: <input type="text" name="thickness" value="5"><br>
 Frame Image: <input type="file" name="frame_image" onchange="fileupload(this.value)"><br>
 Top right corner: <input type="file" name="top_right" onchange="fileupload(this.value)"><br>
-<input type="radio" name="frame" value="Color Frame">Color Frame<br>
-<input type="radio" name="frame" value="Standard Frame">Standard Frame<br>
-<input type="radio" name="frame" value="Image Frame">Image Frame
+<hr>
+* Mandatory
 <hr>
 <input type="submit" value="Load">
 </form>
-<?php 
-/**
- * Creates an input form for image uploads
- */
-function image_uploads() {
-	echo "image_uploads invocation";
+<?php
 }
 
 add_filter('the_content', 'image_uploads');
+add_action( 'the_post', 'change_title' );
 ?>
